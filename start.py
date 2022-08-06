1a01f8909c4d39c9c91ea612340a928fc6885dea
server-python <110650714+server-python@users.noreply.github.com>
2022-08-06 08:18:32 +0430

    Add files via upload

---
+++ start.py
@@ -0,0 +1,326 @@
+import os
+import time
+import random
+from random import choice
+from turtle import color
+
+
+from os import system
+from time import sleep
+system('clear')
+#color
+green = "\033[32m"
+red = "\033[31m"
+blue = "\033[36m"
+pink = "\033[35m"
+yellow = "\033[93m"
+darkblue = "\033[34m"
+white = "\033[00m"
+mark = '\x1b[38;5;4m'
+mark1 = '\x1b[48;5;15m'
+mark2 = '\x1b[0m'
+p=f"""
+{blue}Hello! 
+Me {red}server {darkblue}python 
+{green}or
+{red}server {pink}script {yellow}(:
+
+"""
+c1= random.randrange(1,9)
+c2= random.randrange(1,9)
+c3= random.randrange(1,9)
+c4= random.randrange(1,9)
+c5= random.randrange(1,9)
+c6= random.randrange(1,9)
+c7= random.randrange(1,9)
+c8= random.randrange(1,9)
+c9= random.randrange(1,9)
+c10= random.randrange(1,9)
+c20= random.randrange(1,9)
+c30= random.randrange(1,9)
+c40= random.randrange(1,9)
+c50= random.randrange(1,9)
+c60= random.randrange(1,9)
+c70= random.randrange(1,9)
+c80= random.randrange(1,9)
+c90= random.randrange(1,9)
+c100= random.randrange(1,9)
+c200= random.randrange(1,9)
+c300= random.randrange(1,9)
+c400= random.randrange(1,9)
+c500= random.randrange(1,9)
+c600= random.randrange(1,9)
+c700= random.randrange(1,9)
+c800= random.randrange(1,9)
+c900= random.randrange(1,9)
+c=F"""
+
+({c1}.{c2}.{c3}.{c4}.{c5}.{c6}.{c7}.{c8}.{c9})/({c10}.{c20}.{c30}.{c40}.{c50}.{c60}.{c70}.{c80}.{c90})/({c100}.{c200}.{c300}.{c400}.{c500}.{c600}.{c700}.{c800}.{c900})
+
+"""
+cc1 : list=['/h////e.g/','/h////g.f/','/c////u.g/','/l////s.d/','/t////v.b/','/y////t.s/','/o////p.f/']
+cc2 : list=['yttks34.rest.rubika.ir','sex.porn.xxx.yttks23.rest.rubika',' /(https://Hacked.hack.ir/sex.porn/ir/yttks34.attacked54/rest.rubika.ir/web.rubika.ir/)','porn.sexxxxx.img2.com','yttks34.org.rubika/hacked/@Hacked/.com','allow.code.rubika.sexx.xxx.xxx.porn(3008).ytkss35','localhost://https://rubika.com.org.xxx.co.ir/Hacked','sex.yttks462.192.32.442.34:0883','DDoSer.com/ftps://code.filter.xxx.ico','sxex.xxx.yttks742.py.linux.Hacked.rest.rubika.ir']
+cc3 : list=['187.183.168.64:15111','187.183.221.110:26329','187.183.221.64:26359','187.183.46.154:38189','187.21.194.71:39287','187.21.211.194:26839','187.21.70.179:64934','187.22.15.182:46240','187.22.95.164:39308','187.23.113.125:51818','187.23.113.215:51904','187.23.130.72:14687','187.23.135.197:15570','187.23.135.231:15600','187.23.255.169:17598','187.3.188.120:11915','187.3.68.134:65000','187.37.170.42:14367','187.37.182.133:13488','187.37.227.117:22608','189.100.214.186:35835','189.102.151.54:51829','189.120.67.241:17852','189.121.208.88:36100','110.77.208.252:62225','111.253.91.53:8888']
+
+cc11=(choice(cc1))
+cc22=(choice(cc2))
+cc33=(choice(cc3))
+
+code_filter=f"""
+{mark1}
+{c}#hacker virus{cc11}{cc22}.yttks02.server=({cc33})
+{mark2}
+"""
+
+for i in p:
+    sleep(0.05)
+    print(i,end='',flush=True)
+time.sleep(0.8)
+def start():
+    st=int(input(f"Enter number {red}>>> {yellow}"))
+    if st==0:
+        time.sleep(0.7)
+        nall()
+    if st==1:
+        time.sleep(0.6)
+        yek()
+    if st==2:
+        time.sleep(0.7)
+        do()
+    if st==3:
+        time.sleep(0.6)
+        se()
+    if st==4:
+        time.sleep(0.2)
+        char()
+    if st==00:
+        time.sleep(0.6)
+        sefr()
+def nall():
+    system("clear")
+    for i in code_filter:
+        sleep(0.3)
+        print(i,end='',flush=True)
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def yek():
+    print(f"{green}OK\n\n\n\n{blue}time{yellow}({red}0{yellow}.{darkblue}5 {green}M{yellow})")
+    time.sleep(0.6)
+    code_acc=f"{mark1}(4.2.6.3.7.3.7)/*https://code.rest234.rubika.yttks35.ir/*(3.5.2.7.3.7.3)/king.to.porn.sexxx.xxx/(2.3.6.3.7.3.7.4)*{mark2}"
+    for i in code_acc:
+        sleep(0.03)
+        print(i,end='',flush=True)
+    print("")
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def do():
+    print(f"{green}OK\n\n\n\n{blue}time{yellow}({red}0{yellow}.{darkblue}5 {green}M{yellow})")
+    time.sleep(0.6)
+    code_gap=f"{mark1}(5.3.5.2.5.3.5.2)/*https://sex.porn.yttks736.rest53.rubika-hacked.ir/*(4.2.3.5.3.6.3.6)/sexxx.xxx.porn.sexy/(3.5.3.7.3.7.2.7.2)*{mark2}"
+    for i in code_gap:
+        sleep(0.04)
+        print(i,end='',flush=True)
+    print("")
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def se():
+    print(f"{green}OK\n\n\n\n{blue}time{yellow}({red}0{yellow}.{darkblue}5 {green}M{yellow})")
+    time.sleep(0.6)
+    code_chanel=f"{mark1}(4.2.6.3.7.3.6.2)/*https://yttks.porn.sex.xsexx.org.team-Fuck.com/*(5.3.6.8.3.8.3.8.3)/sex.sexy.org.xxx.porn.sexxx.porn.yttks.rest.pap-code.rubika.ir/(6.3.6.3.6.3.7.3)*{mark2}"
+    for i in code_chanel:
+        sleep(0.04)
+        print(i,end='',flush=True)
+    print("")
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def char():
+    time.sleep(0.8)
+    system("python start.py")
+def sefr():
+    time.sleep(0.7)
+    quit()
+
+ok=f"""
+ {green} ____________________________________________________
+{blue}0{green}|                                                    {green}|
+{blue}1{green}|                                                    {green}|
+{blue}0{green}|                                                    {green}|
+{blue}1{green}|                     {yellow}[{blue}00{yellow}]{red}. Exit                     {green}|
+{blue}0{green}|                                                    {green}|
+{blue}1{green}|                                                    {green}|
+{blue}1{green}|          {yellow}#         server python         #         {green}|
+{blue}0{green}|                                                    {green}|
+{blue}0{green}|          {yellow}[{blue}0{yellow}]{red}. {yellow}Full {red}code!                           {green}|
+{blue}1{green}|          {yellow}[{blue}1{yellow}]{red}. {darkblue}Accont                               {green}|
+{blue}0{green}|          {yellow}[{blue}2{yellow}]{red}. {darkblue}Gap                                  {green}|
+{blue}0{green}|          {yellow}[{blue}3{yellow}]{red}. {darkblue}Chanel                               {green}|
+{blue}1{green}|          {yellow}[{blue}4{yellow}]{red}. {darkblue}restart                              {green}|
+{blue}0{green}|                                                    {green}|
+{blue}0 {green}____________________________________________________
+ {blue}0 0 1 0 1 0 1 0 0 1 0 0 0 1 0 0 0 1 1 0 1 0 1 1 1 0 
+"""
+for i in ok:
+    sleep(0.003)
+    print(i,end='',flush=True)
+print(f"""
+{blue}
+""")
+
+def start():
+    st=int(input(f"Enter number {red}>>> {yellow}"))
+    if st==0:
+        time.sleep(0.7)
+        nall()
+    if st==1:
+        time.sleep(0.6)
+        yek()
+    if st==2:
+        time.sleep(0.7)
+        do()
+    if st==3:
+        time.sleep(0.6)
+        se()
+    if st==4:
+        time.sleep(0.2)
+        char()
+    if st==00:
+        time.sleep(0.6)
+        sefr()
+start()
+c1= random.randrange(1,9)
+c2= random.randrange(1,9)
+c3= random.randrange(1,9)
+c4= random.randrange(1,9)
+c5= random.randrange(1,9)
+c6= random.randrange(1,9)
+c7= random.randrange(1,9)
+c8= random.randrange(1,9)
+c9= random.randrange(1,9)
+c10= random.randrange(1,9)
+c20= random.randrange(1,9)
+c30= random.randrange(1,9)
+c40= random.randrange(1,9)
+c50= random.randrange(1,9)
+c60= random.randrange(1,9)
+c70= random.randrange(1,9)
+c80= random.randrange(1,9)
+c90= random.randrange(1,9)
+c100= random.randrange(1,9)
+c200= random.randrange(1,9)
+c300= random.randrange(1,9)
+c400= random.randrange(1,9)
+c500= random.randrange(1,9)
+c600= random.randrange(1,9)
+c700= random.randrange(1,9)
+c800= random.randrange(1,9)
+c900= random.randrange(1,9)
+c=F"""
+
+({c1}.{c2}.{c3}.{c4}.{c5}.{c6}.{c7}.{c8}.{c9})/({c10}.{c20}.{c30}.{c40}.{c50}.{c60}.{c70}.{c80}.{c90})/({c100}.{c200}.{c300}.{c400}.{c500}.{c600}.{c700}.{c800}.{c900})
+
+"""
+cc1 : list=['/h////e.g/','/h////g.f/','/c////u.g/','/l////s.d/','/t////v.b/','/y////t.s/','/o////p.f/']
+cc2 : list=['yttks34.rest.rubika.ir','sex.porn.xxx.yttks23.rest.rubika',' /(https://Hacked.hack.ir/sex.porn/ir/yttks34.attacked54/rest.rubika.ir/web.rubika.ir/)','porn.sexxxxx.img2.com','yttks34.org.rubika/hacked/@Hacked/.com','allow.code.rubika.sexx.xxx.xxx.porn(3008).ytkss35','localhost://https://rubika.com.org.xxx.co.ir/Hacked','sex.yttks462.192.32.442.34:0883','DDoSer.com/ftps://code.filter.xxx.ico','sxex.xxx.yttks742.py.linux.Hacked.rest.rubika.ir']
+cc3 : list=['187.183.168.64:15111','187.183.221.110:26329','187.183.221.64:26359','187.183.46.154:38189','187.21.194.71:39287','187.21.211.194:26839','187.21.70.179:64934','187.22.15.182:46240','187.22.95.164:39308','187.23.113.125:51818','187.23.113.215:51904','187.23.130.72:14687','187.23.135.197:15570','187.23.135.231:15600','187.23.255.169:17598','187.3.188.120:11915','187.3.68.134:65000','187.37.170.42:14367','187.37.182.133:13488','187.37.227.117:22608','189.100.214.186:35835','189.102.151.54:51829','189.120.67.241:17852','189.121.208.88:36100','110.77.208.252:62225','111.253.91.53:8888']
+
+cc11=(choice(cc1))
+cc22=(choice(cc2))
+cc33=(choice(cc3))
+
+code_filter=f"""
+{mark1}
+{c}#hacker virus{cc11}{cc22}.yttks02.server=({cc33})
+{mark2}
+"""
+
+def nall():
+    system("clear")
+    for i in code_filter:
+        sleep(0.3)
+        print(i,end='',flush=True)
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def yek():
+    print(f"{green}OK\n\n\n\n{blue}time{yellow}({red}0{yellow}.{darkblue}5 {green}M{yellow})")
+    time.sleep(0.6)
+    code_acc=f"{mark1}(4.2.6.3.7.3.7)/*https://code.rest234.rubika.yttks35.ir/*(3.5.2.7.3.7.3)/king.to.porn.sexxx.xxx/(2.3.6.3.7.3.7.4)*{mark2}"
+    for i in code_acc:
+        sleep(0.04)
+        print(i,end='',flush=True)
+    print("")
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def do():
+    print(f"{green}OK\n\n\n\n{blue}time{yellow}({red}0{yellow}.{darkblue}5 {green}M{yellow})")
+    time.sleep(0.6)
+    code_gap=f"{mark1}(5.3.5.2.5.3.5.2)/*https://sex.porn.yttks736.rest53.rubika-hacked.ir/*(4.2.3.5.3.6.3.6)/sexxx.xxx.porn.sexy/(3.5.3.7.3.7.2.7.2)*{mark2}"
+    for i in code_gap:
+        sleep(0.04)
+        print(i,end='',flush=True)
+    print("")
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def se():
+    print(f"{green}OK\n\n\n\n{blue}time{yellow}({red}0{yellow}.{darkblue}5 {green}M{yellow})")
+    time.sleep(0.6)
+    code_chanel=f"{mark1}(4.2.6.3.7.3.6.2)/*https://yttks.porn.sex.xsexx.org.team-Fuck.com/*(5.3.6.8.3.8.3.8.3)/sex.sexy.org.xxx.porn.sexxx.porn.yttks.rest.pap-code.rubika.ir/(6.3.6.3.6.3.7.3)*{mark2}"
+    for i in code_chanel:
+        sleep(0.04)
+        print(i,end='',flush=True)
+    print("")
+    restart=str(input(f"{red}ReStart? {yellow}[{red}y{yellow}/{red}n{yellow}] >>> "))
+    
+    if restart=="y":
+        print("OK")
+        time.sleep(0.7)
+        start()
+    else:
+        quit()
+def char():
+    time.sleep(0.8)
+    system("python start.py")
+def sefr():
+    time.sleep(0.7)
+    quit()
